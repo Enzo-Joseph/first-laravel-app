aad50876f6f3e88cad9998b01e65936e37d7e537
Enzo-Joseph <enzojsphperso@gmail.com>
2024-10-07 19:17:21 +0700

    Added pagination

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,20 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    $posts = Post::latest();
-
-    if (request('search')) {
-        $posts->where('title', 'like', '%' . request('search') . '%');
-    }
-
-    return view('blog', ['title' => 'Blog Page', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
+    return view('blog', ['title' => 'Blog Page', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(10)->withQueryString()]);
 });
 
 Route::get('/blog/{post:slug}', function (Post $post) {
     return view('blog-post', ['title' => 'Single Post', 'post' => Post::find($post->id)]);
 });
 
-Route::get('/author/{user:username}', function (User $user) {
-    return view('blog', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
-});
+// Route::get('/author/{user:username}', function (User $user) {
+//     return view('blog', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
+// });
 
-Route::get('/category/{category:slug}', function (Category $category) {
-    return view('blog', ['title' => 'Articles in ' . $category->name, 'posts' => $category->posts]);
-});
+// Route::get('/category/{category:slug}', function (Category $category) {
+//     return view('blog', ['title' => 'Articles in ' . $category->name, 'posts' => $category->posts]);
+// });
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
